27ba9582492f202ab1244d3410333c23df2ccadd
Joel Garrido <joelgamar@gmail.com>
2022-11-16 15:40:34 +0100

    gson serializer

--- app/src/main/java/com/joelgh/app/commons/KSerializer.kt
+++ app/src/main/java/com/joelgh/app/commons/KSerializer.kt
@@ -1,6 +1,18 @@
 package com.joelgh.app.commons
 
+import com.google.gson.Gson
+
 interface KSerializer {
     fun<T> toJson(src: T, srcClass: Class<T>): String
     fun<T> fromJson(src: String, srcClass: Class<T>): T
+}
+
+class GsonSerializer : KSerializer{
+
+    private val gson = Gson()
+
+    override fun <T> toJson(src: T, srcClass: Class<T>): String = gson.toJson(src, srcClass)
+
+    override fun <T> fromJson(src: String, srcClass: Class<T>): T = gson.fromJson(src, srcClass)
+
 }
