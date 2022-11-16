a25a5d1d5b80ddf73d925aaad0d391de4b65048e
Joel Garrido <joelgamar@gmail.com>
2022-11-16 16:54:43 +0100

    fixed minor issues

--- app/src/main/java/com/joelgh/features/MainActivity.kt
+++ app/src/main/java/com/joelgh/features/MainActivity.kt
@@ -2,10 +2,12 @@ package com.joelgh.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joelgh.rss_aggregator.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
