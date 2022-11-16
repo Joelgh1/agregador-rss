4d01b7e9fa0d60693647b213c488a7b2b9e24378
Joel Garrido <joelgamar@gmail.com>
2022-11-16 15:51:49 +0100

    repository

--- app/src/main/java/com/joelgh/features/rss_management/data/RssDataRepository.kt
+++ app/src/main/java/com/joelgh/features/rss_management/data/RssDataRepository.kt
@@ -1,15 +1,16 @@
 package com.joelgh.features.rss_management.data
 
+import com.joelgh.features.rss_management.data.local.LocalDataSource
 import com.joelgh.features.rss_management.domain.Rss
 import com.joelgh.features.rss_management.domain.RssRepository
 
-class RssDataRepository : RssRepository {
+class RssDataRepository(private val localSource: LocalDataSource) : RssRepository {
 
-    override fun create(rss: Rss) {
-        TODO("Not yet implemented")
-    }
+    override fun create(rss: Rss) = localSource.create(rss)
 
     override fun delete(rss: Rss) {
         TODO("Not yet implemented")
     }
+
+    override fun getAll(): List<Rss> = localSource.getAll()
 }
--- app/src/main/java/com/joelgh/features/rss_management/domain/RssRepository.kt
+++ app/src/main/java/com/joelgh/features/rss_management/domain/RssRepository.kt
@@ -3,4 +3,5 @@ package com.joelgh.features.rss_management.domain
 interface RssRepository {
     fun create(rss: Rss)
     fun delete(rss: Rss)
+    fun getAll(): List<Rss>
 }
