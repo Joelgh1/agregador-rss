6d22d75408dc76e74c9383659a1e64e773c7ecca
Joel Garrido <joelgamar@gmail.com>
2022-11-21 19:07:29 +0100

    bottom navigation menu and navigation

--- app/src/main/java/com/joelgh/features/MainActivity.kt
+++ app/src/main/java/com/joelgh/features/MainActivity.kt
@@ -2,12 +2,43 @@ package com.joelgh.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.joelgh.rss_aggregator.R
+import com.joelgh.rss_aggregator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setUpBinding()
+        setUpNavigation()
+        //findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.page_2
     }
 
+    override fun onStart() {
+        super.onStart()
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.page_2
+    }
+
+    private fun setUpBinding(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+
+    private fun setUpNavigation(){
+        binding?.bottomNavigation?.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.page_1 -> findNavController(R.id.nav_host_fragment).navigate(R.id.rssManagementFragment)
+                R.id.page_2 -> findNavController(R.id.nav_host_fragment).navigate(R.id.rssFeedFragment)
+                R.id.page_3 -> findNavController(R.id.nav_host_fragment).navigate(R.id.profileFragment)
+            }
+            true
+        }
+    }
 }
