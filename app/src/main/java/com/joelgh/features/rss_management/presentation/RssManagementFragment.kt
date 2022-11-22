381f7ecda3a6d4664928bbe5b7ffadc369fbf302
Joel Garrido <joelgamar@gmail.com>
2022-11-22 16:50:16 +0100

    toolbar

--- app/src/main/java/com/joelgh/features/rss_management/presentation/RssManagementFragment.kt
+++ app/src/main/java/com/joelgh/features/rss_management/presentation/RssManagementFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
 import com.joelgh.rss_aggregator.R
 import com.joelgh.rss_aggregator.databinding.FragmentRssManagementBinding
 
