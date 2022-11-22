154aca485cf3650c8bdb5233208f237a0a3868ba
Joel Garrido <joelgamar@gmail.com>
2022-11-22 16:22:32 +0100

    menu

--- app/src/main/java/com/joelgh/features/rss_management/presentation/RssManagementFragment.kt
+++ app/src/main/java/com/joelgh/features/rss_management/presentation/RssManagementFragment.kt
@@ -6,21 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.joelgh.rss_aggregator.R
+import com.joelgh.rss_aggregator.databinding.FragmentRssManagementBinding
 
 class RssManagementFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+    private var binding: FragmentRssManagementBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rss_management, container, false)
+        binding = FragmentRssManagementBinding.inflate(inflater, container, false)
+        setUpToolBar()
+        return binding?.root
+    }
+
+    private fun setUpToolBar(){
+        binding?.apply {
+            rssManagementToolBar.inflateMenu(R.menu.mangement_tool_bar_menu)
+        }
     }
 
 }
