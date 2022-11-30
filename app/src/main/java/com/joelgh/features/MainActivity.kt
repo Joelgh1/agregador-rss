da087250e6ec103b2aec4ca4a482ba9b554760ea
Joelgh <joelgamar@gmail.com>
2022-12-01 00:29:20 +0100

    fixed issues

--- app/src/main/java/com/joelgh/features/MainActivity.kt
+++ app/src/main/java/com/joelgh/features/MainActivity.kt
@@ -2,21 +2,12 @@ package com.joelgh.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.joelgh.app.commons.GsonSerializer
-import com.joelgh.features.rss_management.data.RssDataRepository
-import com.joelgh.features.rss_management.data.local.XmlLocalDataSource
 import com.joelgh.rss_aggregator.R
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val repo = RssDataRepository(XmlLocalDataSource(this, GsonSerializer()))
-        GlobalScope.launch {
-            repo.create("RssPrueba", "UrlPrueba")
-        }
     }
 
 }
