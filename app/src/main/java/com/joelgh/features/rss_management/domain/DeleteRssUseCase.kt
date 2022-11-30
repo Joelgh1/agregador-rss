789151e9c7cd40a3942ba00f0acd228686ab5703
Joel Garrido <joelgamar@gmail.com>
2022-11-30 15:47:49 +0100

    delete rss domain layer

---
+++ app/src/main/java/com/joelgh/features/rss_management/domain/DeleteRssUseCase.kt
@@ -0,0 +1,11 @@
+package com.joelgh.features.rss_management.domain
+
+import com.joelgh.app.commons.error_management.Either
+import com.joelgh.app.commons.error_management.ErrorApp
+
+class DeleteRssUseCase(private val repository: RssRepository) {
+
+    suspend fun execute(name: String): Either<ErrorApp, Boolean>{
+        return repository.delete(name)
+    }
+}
