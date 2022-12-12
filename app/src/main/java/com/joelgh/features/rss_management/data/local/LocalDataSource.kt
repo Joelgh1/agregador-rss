e2f4d4937b2f9a7f19f6f2d6e96371f8f7ae20c8
Joel Garrido <joelgamar@gmail.com>
2022-12-12 19:40:07 +0100

    Data store

--- app/src/main/java/com/joelgh/features/rss_management/data/local/LocalDataSource.kt
+++ app/src/main/java/com/joelgh/features/rss_management/data/local/LocalDataSource.kt
@@ -3,9 +3,10 @@ package com.joelgh.features.rss_management.data.local
 import com.joelgh.app.commons.error_management.Either
 import com.joelgh.app.commons.error_management.ErrorApp
 import com.joelgh.features.rss_management.domain.Rss
+import kotlinx.coroutines.flow.Flow
 
 interface LocalDataSource {
     suspend fun create(rss: Rss): Either<ErrorApp, Boolean>
-    suspend fun getAll(): Either<ErrorApp, List<Rss>>
+    suspend fun getAll(): Flow<Either<ErrorApp, List<Rss>>>
     suspend fun delete(name: String): Either<ErrorApp, Boolean>
 }
